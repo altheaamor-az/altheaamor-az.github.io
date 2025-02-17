e5576055fe4df810cb0e7a642e35a9c73265c526
altheaamor-az <altheaamor.azures@jpschools.org>
2025-02-17 17:03:18 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,31 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
+    createPlatform(50, 700, 50, 50, "red");
+createPlatform(120,600, 50, 60, "orange");
+createPlatform(200, 620, 30,300, "yellow");
+createPlatform(270, 559, 50, 290, "green");
+createPlatform(340,500, 50, 60, "indigo");
+createPlatform(440, 490, 1300, 20, "blue");
+createPlatform(1000,410, 50, 60, "brown");
 
 
     // TODO 3 - Create Collectables
+    createCollectable("grace", 350, 450, 0.1, 0.8);
+createCollectable("kennedi", 200, 170, 0.5, 0.7);
+createCollectable("max", 440, 300, 0.1, 0.8);
 
 
 
     
     // TODO 4 - Create Cannons
-
+    createCannon("right", 500, 2000);
+    createCannon("bottom", 500, 2000);
+    createCannon("bottom", 900, 2000)
 
     
     
